0673527508a24ba748981aaaffbc245cc94fee96
samarth <samthedude4321@gmail.com>
2025-12-24 00:13:35 +0530

    fixes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const { auth } = require('../middleware/auth');
 
 const router = express.Router();
 
